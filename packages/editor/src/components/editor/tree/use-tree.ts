969fcdfdc20d116a0bf49f08c4876a97efc980cf
lvliqi <lvliqi_d@163.com>
2025-08-26 09:27:12 +0800

    feat:up

--- packages/editor/src/components/editor/tree/use-tree.ts
+++ packages/editor/src/components/editor/tree/use-tree.ts
@@ -5,70 +5,43 @@ import { v4 as uuidv4 } from 'uuid';
 export const useTree = () => {
   const { playground } = useStore()!;
 
-  const calculateComponent = (component: Component) => {
-    const props = {
+  const calculateComponent = (component: Component, slotName?:string) => {
+    const props = Object.entries(component.props).map(([key, value]) => ({
       id: uuidv4(),
-      label: 'props',
+      label: `props::${key}`,
       data: {
-        type: 'props',
-        data: component.props
-      },
-      children: Object.entries(component.props).map(([key, value]) => ({
-        id: uuidv4(),
-        label: key,
-        data: {
-          type: 'prop',
-          data: value
-        }
-      }))
-    };
+        type: 'prop',
+        data: value
+      }
+    }));
 
-    const directives = {
+    const directives = Object.entries(component.directives).map(([key, value]) => ({
       id: uuidv4(),
-      label: 'directives',
+      label: `directives::${key}`,
       data: {
-        type: 'directives',
-        data: component.directives
-      },
-      children: Object.entries(component.directives).map(([key, value]) => ({
-        id: uuidv4(),
-        label: key,
-        data: {
-          type: 'directive',
-          data: value
-        }
-      }))
-    };
+        type: 'directive',
+        data: value
+      }
+    }));
 
-    const slots = {
-      id: uuidv4(),
-      label: 'slots',
-      data: {
-        type: 'slots',
-        data: component.slots
-      },
-      children: Object.entries(component.slots).map(([key, value]) => ({
-        id: uuidv4(),
-        label: key,
-        data: {
-          type: 'slot',
-          data: value
-        },
-        children: value.map(slot => calculateComponent(slot))
-      }))
-    };
+    const slots = Object.entries(component.slots).reduce<any[]>((acc, [key, value]) => {
+      {
+        acc.push(...value.map(com => calculateComponent(com, key)));
+        return acc;
+      }
+    }, []);
 
     return {
       id: uuidv4(),
-      label: component.name,
+      label: `${slotName ? `slots::${slotName}::` : ''}${component.name}-${component.$class}`,
       data: {
         type: 'component',
         data: component
       },
       children: [
-        ...(props.children.length > 0 ? [props] : []),
-        ...(directives.children.length > 0 ? [directives] : []),
-        ...(slots.children.length > 0 ? [slots] : [])
+        ...props,
+        ...directives,
+        ...slots
       ]
     };
   };
