88630ba7137acaeeea3b23232ccbb07d90079722
lvliqi <lvliqi_d@163.com>
2025-08-27 16:44:55 +0800

    feat: up

--- packages/action/src/models/action-struct-transform.ts
+++ packages/action/src/models/action-struct-transform.ts
@@ -11,11 +11,11 @@ export class ActionStructTransform extends Action<TExtension> {
     this.extension.ruleString = '';
   }
 
-  async handle(params: any[], options): Promise<any> {
+  handle(params: any[], options) {
     if (this.params.length === 0) return;
     const paramValues = this.transformParams(params, options);
     const obj = paramValues[0];
-    const result = await jsonata(this.extension.ruleString).evaluate(obj);
+    const result = jsonata(this.extension.ruleString).evaluate(obj);
     console.log('result', result);
     
     this.setResultData(params, options, result);
